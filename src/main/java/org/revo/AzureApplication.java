befec01ea20e966b4ddda3563eb76b4d95f0294a
ashraf <ashraf1abdelrasool@gmail.com>
2016-10-31 15:53:41 +0200

    ya rabhhb

--- src/main/java/org/revo/AzureApplication.java
+++ src/main/java/org/revo/AzureApplication.java
@@ -6,6 +6,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.nio.file.FileStore;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.text.NumberFormat;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 @SpringBootApplication
 public class AzureApplication {
@@ -19,6 +26,22 @@ public class AzureApplication {
 class web {
     @GetMapping
     public String index(HttpServletRequest request) {
-        return "hello   azure    {path }" + request.getServletContext().getRealPath("");
+
+        NumberFormat nf = NumberFormat.getNumberInstance();
+
+
+        return StreamSupport.stream(FileSystems.getDefault().getRootDirectories().spliterator(), false).map(path -> {
+            String t = "";
+            FileStore store = null;
+            try {
+                store = Files.getFileStore(path);
+                t += path + " : " + "available=" + nf.format(store.getUsableSpace())
+                        + ", total=" + nf.format(store.getTotalSpace());
+            } catch (IOException e) {
+
+            }
+            return t;
+        }).collect(Collectors.joining("\n"));
+
     }
 }
