43b9b71dc52bd4a87084d7c821d1ac0cf6d3ba30
ashraf <ashraf1abdelrasool@gmail.com>
2016-10-31 02:57:06 +0200

    ya rabhh

--- src/main/java/org/revo/AzureApplication.java
+++ src/main/java/org/revo/AzureApplication.java
@@ -5,17 +5,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @SpringBootApplication
 public class AzureApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AzureApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AzureApplication.class, args);
+    }
 }
+
 @RestController
-class web{
-	@GetMapping
-	public String index(){
-		return "hello   azure";
-	}
+class web {
+    @GetMapping
+    public String index(HttpServletRequest request) {
+        return "hello   azure    {path }" + request.getServletContext().getRealPath("");
+    }
 }
